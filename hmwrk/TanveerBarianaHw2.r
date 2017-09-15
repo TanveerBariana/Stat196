367e9269ba99f35773c6185102a53282c3b7e3e4
tanveer B <tanveerbariana@gmail.com>
2017-09-14 17:13:04 -0700

    initial hmw2 push

---
+++ hmwrk/TanveerBarianaHw2.r
@@ -0,0 +1,23 @@
+#hmwrk2
+smallTen <- NULL
+loc<- NULL
+#1
+#a
+x <- c(2, 10, 57, NA, 7.6, NA, 200, 9)
+#b
+for (i in 1:length(x)) {
+    if (x[i] < 10) {
+        smallTen <- c(smallTen, x[i])
+#c
+        loc<- c(loc, i)
+    }
+}
+#d
+sd(x)
+#e
+sum(is.na(x))
+#f
+na.omit(x)
+is.na(x)
+!is.na(x)
+#! is useful if you wnated to loop through teh vecor indexes and add the elements to a new vector if they were not NA
