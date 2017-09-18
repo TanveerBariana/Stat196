feec4a46f3ed6eb77d4ddf49789d19a59b2480bf
TanveerBariana <tanveerbariana@gmail.com>
2017-09-18 14:48:08 -0700

    Add files via upload

---
+++ classwrk/9 18 17.R
@@ -0,0 +1,38 @@
+#9/18/2017
+mymat<- matrix(1:9, nrow = 3, ncol = 3)
+mymat
+mymat2<- matrix(1:9, nrow = 3, byrow = T)
+mymat2
+mymat3<- matrix(NA, 2, 2)
+mymat3[1, 1]<- 6
+
+y<- matrix(c(2, 4, 3, 1), 2)
+y
+x<- matrix(c(3, 2, 0, 1), 2)
+x
+5*x
+x*y
+x%*%y
+x
+y
+#ex
+z<- matrix(c(1,1,2,4,6,3,7,8,7), nrow = 3, byrow = T)
+z
+#1
+z.i<- solve(z)
+#2
+z %*% z.i
+#3
+z^2
+z
+
+rowMean<- apply(z, 1, mean)
+rowMean
+colSum <- apply(z, 2, sum)
+colSum
+bigMat<- apply(z, 1:3)
+
+#central limit ex
+sample <- runif(5)
+mean(sample)
+ 
--- hmwrk/TanveerBarianaHw2.r
+++ hmwrk/TanveerBarianaHw2.r
@@ -6,11 +6,11 @@ loc<- NULL
 x <- c(2, 10, 57, NA, 7.6, NA, 200, 9)
 #b
 for (i in 1:length(x)) {
-    if (x[i] < 10) {
-        smallTen <- c(smallTen, x[i])
-#c
-        loc<- c(loc, i)
-    }
+  if (x[i] < 10) {
+    smallTen <- c(smallTen, x[i])
+    #c
+    loc<- c(loc, i)
+  }
 }
 #d
 sd(x)
@@ -21,3 +21,12 @@ na.omit(x)
 is.na(x)
 !is.na(x)
 #! is useful if you wnated to loop through teh vecor indexes and add the elements to a new vector if they were not NA
+
+#2
+xi <- 1 
+n<- 3
+for(i in 1:n+1){
+  xi<- ((xi^2)+9)/(2*xi)
+}
+xi
+17/5
