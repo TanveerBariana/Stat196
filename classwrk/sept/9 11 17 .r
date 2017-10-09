f75fda993c0a417a1e0d2def66809461aa337181
tanveer B <tanveerbariana@gmail.com>
2017-10-09 14:10:15 -0700

    fix clean

---
+++ classwrk/sept/9 11 17 .r
@@ -0,0 +1,23 @@
+x<- c(8, 1, 22, 3, 7)
+#a
+if(x[3]<10){
+  print("yes")
+}else{
+  print("no")
+}
+#b
+for(i in 1:length(x)){
+  if(x[i]<10){
+    print(x[i])
+  }else{
+    print("not smaller than 10")
+  }
+}
+#c
+smallnums<- NULL
+for(i in 1:length(x)){
+  if(x[i]<10){
+   smallnums<- c(smallnums, x[i])
+  }
+}
+#2
