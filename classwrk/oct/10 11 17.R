8dc3faeeab8d990fb44feb206005c74710cf0dc0
TanveerBariana <tanveerbariana@gmail.com>
2017-10-13 14:36:34 -0700

    Add files via upload

--- classwrk/oct/10 11 17.R
+++ classwrk/oct/10 11 17.R
@@ -21,3 +21,29 @@ driveSpeed<- c(40, 30, 50, 45, 55, 20, 60)
 Car.c<- c('r', 'b', 'r', 'b', 'b', 'b', 'r')
 data<- data.frame(cbind(names, driveSpeed, Car.c))
 data[order(data$driveSpeed),]
+
+#cleaning up pima data set
+?Pima.tr
+?Pima.te
+View(Pima.tr)
+library(tree)
+pima.tree<- tree(formula= type~. , data = Pima.tr)
+pima.tree
+summary(pima.tree)
+plot(pima.tree)
+text(pima.tree)
+#sort data
+Pima.tr[order(Pima.tr$glu), ]
+
+under123<- subset(Pima.tr, glu < 123.5)
+table(under123$type)
+over123<- subset(Pima.tr, glu >= 123.5)
+table(over123$type)
+summary(pima.tree)
+str(Pima.te)
+pima.test<- predict(pima.tree, Pima.te, type = 'class')
+pima.test
+cbind(pima.test, Pima.te$type)
+table(pima.test, Pima.te$type)
+(44+48)/332
+10/332
