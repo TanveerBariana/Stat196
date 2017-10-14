9945915ee0f1fa485042876d92dac7c14b073e7d
tanveer B <tanveerbariana@gmail.com>
2017-10-14 15:51:05 -0700

    read_xls fucking WORKS

---
+++ hmwrk/TanveerBarianaHw4.r
@@ -0,0 +1,9 @@
+#tanveer bariana
+#homework 4?
+#1
+#a
+install.packages("tidyverse")
+library(readxl)
+dat_Data <- read_xlsx("C:\\Users\\tanve\\Desktop\\dataSci\\stat196\\data\\deptOfEducationFundingbyState.xlsx")
+#b
+rownames(state.x77)
