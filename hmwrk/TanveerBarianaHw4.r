8a4442e56300e7a34a445e8755bd3c3a97ecc606
tanveer B <tanveerbariana@gmail.com>
2017-10-15 19:21:49 -0700

    doen with number one

--- hmwrk/TanveerBarianaHw4.r
+++ hmwrk/TanveerBarianaHw4.r
@@ -1,9 +1,27 @@
 #tanveer bariana
 #homework 4?
-#1
-#a
+#1%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#a--------------------------------------------------------
 install.packages("tidyverse")
 library(readxl)
 dat_Data <- read_xlsx("C:\\Users\\tanve\\Desktop\\dataSci\\stat196\\data\\deptOfEducationFundingbyState.xlsx")
-#b
+#b----------------------------------------------------------
 rownames(state.x77)
+summary(state.x77)
+dem_Data <- merge(dat_Data, state.x77,by.x= 1,  by.y= 0)
+View(dat_Data)
+View(state.x77)
+View(dem_Data)
+#c------------------------------------------------------------
+d<- data.matrix(dem_Data[,-1])
+View(d)
+maybe_thisl_matter<- cor(d)
+## it appaers that area and life expectancy correlate to the federal funds the most with respective values -0.43 and 0.2029
+#d-----------------------------------------------------------------
+install.packages("corrplot")
+library(corrplot)
+corrplot(maybe_thisl_matter, method = "number", type = 'upper')
+#or
+corrplot.mixed(maybe_thisl_matter, lower = "number", upper = 'circle')
+
+#2
