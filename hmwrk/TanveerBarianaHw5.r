60740fb1639bf4da2163e108141b6e0b760ab11e
Tanveer Bariana <tanveerbariana@gmail.com>
2017-10-24 20:02:16 -0700

    1abcd

--- hmwrk/TanveerBarianaHw5.r
+++ hmwrk/TanveerBarianaHw5.r
@@ -5,15 +5,16 @@
 #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     #p(5 in 30) p(l)= .1
 plefty <- choose(30, 5) * (.1 ^ 5) * (.9 ^ 25)
-binom <- function(n,r, p) {
+binom <- function(n,k,r, p) {
     ans<-NULL
-    for (r in r:n) {
+    for (r in r:k) {
         ans[r]<- choose(n, r) * (p ^ r) * ((1-p) ^ (n-r))
     }
     return(ans)
 }
 #bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-pdistX<- binom(30,1,.1)
+pdistX<- binom(30,30,1,.1)
 #ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 plot(pdistX, type= 'h')
 #ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+pLeftChair <- sum(binom(30, 3, 1, .1))
