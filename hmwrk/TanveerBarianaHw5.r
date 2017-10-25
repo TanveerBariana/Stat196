f3bd6a32a317eaac152eff3148e5a36d370ffa50
Tanveer Bariana <tanveerbariana@gmail.com>
2017-10-24 20:30:28 -0700

    like all of 2

--- hmwrk/TanveerBarianaHw5.r
+++ hmwrk/TanveerBarianaHw5.r
@@ -5,16 +5,27 @@
 #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     #p(5 in 30) p(l)= .1
 plefty <- choose(30, 5) * (.1 ^ 5) * (.9 ^ 25)
-binom <- function(n,k,r, p) {
-    ans<-NULL
-    for (r in r:k) {
-        ans[r]<- choose(n, r) * (p ^ r) * ((1-p) ^ (n-r))
-    }
-    return(ans)
-}
+dbinom(5, 30, .1)
 #bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-pdistX<- binom(30,30,1,.1)
+pdistX<- dbinom(1:30, 30, .1)
 #ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 plot(pdistX, type= 'h')
 #ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-pLeftChair <- sum(binom(30, 3, 1, .1))
+pLeftChair <- pbinom(4, 30, .1)
+#22222222222222222222222222222222222222222222222222222222222222222222222
+#mean 7, sd 1.2
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+overNine <- 1 - pnorm(9, 7, 1.2)
+#bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+nice <- pnorm(8, 7, 1.2) - pnorm(6, 7, 1.2)
+#ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+qnorm(.85, 7, 1.2)
+#ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+sam <- sample(5.8:9.2, 10, T)
+mean(sam)
+sd(sam)
+#eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+saem <- sample(5.8:9.2, 10000, T)
+mean(saem)
+sd(saem)
+#33333333333333333333333333333333333333333333333333333333333333333333333
