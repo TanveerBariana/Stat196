10c1e45f68fcd7c5c3a4142f8cc6989a180b84fd
tanveer B <tanveerbariana@gmail.com>
2017-11-01 14:12:28 -0700

    hmwrk6 more

--- hmwrk/TanveerBarianaHw6.r
+++ hmwrk/TanveerBarianaHw6.r
@@ -2,10 +2,13 @@
 #homework 6
 #----------------------------------1------------------------------------------------
 #---------------a-------------------
-install.packages("tidyr")
-library("read.xls")
+install.packages("read_xls")
+library("readxl")
 cards <- read_xls(path = "C:\\Users\\tanve\\Desktop\\dataSci\\stat196\\data\\cards.xls")
 heads <- cards[1,]
 colnames(cards) <- heads
 cards<- cards[-1,]
 str(cards)
+temp.col <- c(3:5, 25)
+cards[,temp.col]<-lapply(cards[ ,temp.col],factor)
+#sex, education, marriage, default pay
