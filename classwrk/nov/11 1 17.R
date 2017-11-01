4c00bdc38ee6c73d0262d33e2074d07571cb9e00
tanveer B <tanveerbariana@gmail.com>
2017-11-01 14:46:26 -0700

    11 1 17

--- classwrk/nov/11 1 17.R
+++ classwrk/nov/11 1 17.R
@@ -1,1 +1,8 @@
 #11/1/17
+#table(preds, truth= testset$default)
+#attach() : puts vector into data path allowing you to not have to use vector$nam, just name
+plot(1:25, rep(1, 25), pch = 1)
+plot(1:25, rep(1, 25), pch = 1:25)
+plot(1:25, rep(1, 25), pch = 1:25, col = 1:25)
+#locator :use it find shit?
+#
