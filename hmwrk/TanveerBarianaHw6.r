94156e8867932f0099b305cbd68d0dfc234d2344
tanveer B <tanveerbariana@gmail.com>
2017-11-01 14:45:52 -0700

    hmwrk6 even more

--- hmwrk/TanveerBarianaHw6.r
+++ hmwrk/TanveerBarianaHw6.r
@@ -12,3 +12,12 @@ str(cards)
 temp.col <- c(3:5, 25)
 cards[,temp.col]<-lapply(cards[ ,temp.col],factor)
 #sex, education, marriage, default pay
+#---------------b-------------------
+set.seed(42)
+#---------------c-------------------
+test <- cards[which(cards$SEX == 1),]
+train <- cards[-which(cards$SEX == 1),]
+#---------------d-------------------
+library("tree")
+test.tree <- tree(test[, -1])
+?tree
