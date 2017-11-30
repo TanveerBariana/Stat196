7f2893d8cffedc5622b3387ec767d31ff3df8fd6
tanveer B <tanveerbariana@gmail.com>
2017-11-30 00:03:56 -0800

    well th report was bad

--- hmwrk/TermProject.r
+++ hmwrk/TermProject.r
@@ -1,1 +1,12 @@
 wiki_data_raw <- read.csv(file = "C:\\Users\\tanve\\Desktop\\dataSci\\stat196\\data\\WikiData.csv")
+str(wiki_data_raw)
+sum(unique(wiki_data_raw$timestamp))
+date(wiki_data_raw$timestamp)
+date()
+
+#lets see if we can cut down the number of times it consideres unique down to just caring about the day
+wiki_data_dates <- wiki_data_raw
+#get rid of last 6 digits that mean teh hour second and minute
+wiki_data_dates$timestamp <- (wiki_data_raw$timestamp) / 100000
+length(unique(wiki_data_dates$timestamp))
+length(unique(wiki_data_raw$timestamp))
