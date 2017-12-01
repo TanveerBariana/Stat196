57a903259881976b9a5c8162e4a1ab9079e25ba4
tanveer B <tanveerbariana@gmail.com>
2017-11-30 18:33:02 -0800

    and the adventure begins

--- hmwrk/TermProject.r
+++ hmwrk/TermProject.r
@@ -14,7 +14,21 @@ date(wiki_data_raw$timestamp)
 #   add togetehr and avg again 
 wiki_data_dates <- wiki_data_raw
 wiki_data_dates$timestamp <- (wiki_data_raw$timestamp) / 100000
-length(unique(wiki_data_dates$timestamp))
+#take the totol number of clicks on search results and divide it by the number of days to get the avg daily propertion 
+sum(wiki_data_raw$action == 'visitPage') /length(unique(wiki_data_dates$timestamp))
 length(unique(wiki_data_raw$timestamp))
 day_avgs <- mean(wiki_data_dates[])
 tempy <- c(unique(wiki_data_dates$timestamp))
+
+#2 Which results do people tend to try first? How does it change day-to-day?
+#what do they try first 
+#   make vector of uniques sessions
+#   cut that down to just teh first instance of visitPage
+#   get result num 
+#change day by day
+#   graph with y= result num, x= day 
+wiki_data_search<- wiki_data_raw$action== 
+
+#3 What is our daily overall zero results rate ? How does it vary between the groups ?
+#4 Let session length be approximately the time between the first event and the last event in a session. Choose a variable from the dataset and describe its relationship to session length. Visualize the relationship.
+#5 Summarize your findings in an executive summary.
