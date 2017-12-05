b95a110e066f7be2bd1cff22b7afa3f34d78b7c3
tanveer B <tanveerbariana@gmail.com>
2017-12-04 20:47:13 -0800

    here is bette rgame

--- hmwrk/TermProject.r
+++ hmwrk/TermProject.r
@@ -1,25 +1,40 @@
 wiki_data_raw <- read.csv(file = "C:\\Users\\tanve\\Desktop\\dataSci\\stat196\\data\\WikiData.csv")
+library('data.table')
+library('ggplot2')
+wiki_data_raw<- data.table(wiki_data_raw)
 str(wiki_data_raw)
-sum(unique(wiki_data_raw$timestamp))
-date(wiki_data_raw$timestamp)
 #1 What is our daily overall clickthrough rate? How does it vary between the groups?
-#what is teh daily click rate
-#   how many days are there
-#   how many instances of visitPage occured in that time 
-#   whats the difference in groups
-#possible structure
-#   split by group
-#   take avg of each group
-#   present difference 
-#   add togetehr and avg again 
-wiki_data_dates <- wiki_data_raw
-wiki_data_dates$timestamp <- (wiki_data_raw$timestamp) / 100000
-#take the totol number of clicks on search results and divide it by the number of days to get the avg daily propertion 
-sum(wiki_data_raw$action == 'visitPage') /length(unique(wiki_data_dates$timestamp))
-length(unique(wiki_data_raw$timestamp))
-day_avgs <- mean(wiki_data_dates[])
-tempy <- c(unique(wiki_data_dates$timestamp))
-
+wiki_data_dates <- wiki_data_raw[which(wiki_data_raw$action == 'visitPage' | wiki_data_raw$action == 'searchResultPage'),]
+wiki_data_dates$timestamp <- as.integer(wiki_data_dates$timestamp / 1000000)
+wiki_data_dates$timestamp<- as.factor(wiki_data_dates$timestamp)
+dates <- (unique(wiki_data_dates$timestamp))
+daily <- NULL
+for (i in 1:length(dates)) {
+    daily$Aclicks[i] <- length(which(wiki_data_dates$action == 'visitPage' & wiki_data_dates$timestamp == dates[i] & wiki_data_dates$group=='a'))
+    daily$Asearchs[i] <- length(which(wiki_data_dates$action == 'searchResultPage' & wiki_data_dates$timestamp == dates[i] & wiki_data_dates$group == 'a'))
+    daily$Bclicks[i] <- length(which(wiki_data_dates$action == 'visitPage' & wiki_data_dates$timestamp == dates[i] & wiki_data_dates$group == 'b'))
+    daily$Bsearchs[i] <- length(which(wiki_data_dates$action == 'searchResultPage' & wiki_data_dates$timestamp == dates[i] & wiki_data_dates$group == 'b'))
+}
+daily$Aavg <- daily$Aclicks / daily$Asearchs
+daily$Bavg <- daily$Bclicks / daily$Bsearchs
+daily$avg <- (daily$Aclicks + daily$Bclicks) / (daily$Asearchs + daily$Bsearchs)
+str(daily)
+daily <- data.frame(daily)
+daily$date<- dates
+rownames(daily) <- as.character(dates)
+View(daily)
+clickthrough <- ggplot(data = daily, aes(x = daily$date, y = daily$avg))
+clickthrough <- clickthrough + geom_bar(aes(fill= daily$date), stat = 'identity', color = 'white', alpha = 0.4)
+clickthrough <- clickthrough + ggtitle('Over all daily Avg for each date') + theme_classic() + scale_fill_brewer(palette = "PuRd")
+clickthrough
+clickthroughA <- ggplot(data = daily, aes(x = daily$date, y = daily$Aavg))
+clickthroughA <- clickthroughA + geom_bar(aes(fill = daily$date), stat = 'identity', color = 'white', alpha = 0.4)
+clickthroughA <- clickthroughA + ggtitle('Daily Avg for group A on each date') + theme_classic() + scale_fill_brewer(palette = "PuRd")
+clickthroughA
+clickthroughB <- ggplot(data = daily, aes(x = daily$date, y = daily$Bavg))
+clickthroughB <- clickthroughB + geom_bar(aes(fill = daily$date), stat = 'identity', color = 'white', alpha = 0.4)
+clickthroughB <- clickthroughB + ggtitle('Daily Avg for group B on each date') + theme_classic() + scale_fill_brewer(palette = "PuRd")
+clickthroughB
 #2 Which results do people tend to try first? How does it change day-to-day?
 #what do they try first 
 #   make vector of uniques sessions
@@ -27,7 +42,7 @@ tempy <- c(unique(wiki_data_dates$timestamp))
 #   get result num 
 #change day by day
 #   graph with y= result num, x= day 
-wiki_data_search<- wiki_data_raw$action== 
+wiki_data_search <- data.table(wiki_data_raw)
 
 #3 What is our daily overall zero results rate ? How does it vary between the groups ?
 #4 Let session length be approximately the time between the first event and the last event in a session. Choose a variable from the dataset and describe its relationship to session length. Visualize the relationship.
